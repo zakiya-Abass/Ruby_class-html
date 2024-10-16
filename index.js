c7b6ec1618c50d2009a903f99f570423f5e25081
zakiya <zakiyaabass@icloud.com>
2024-10-16 09:51:11 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const colorButton = document.getElementById("colorButton");
+colorButton.addEventListener('click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor==='blue'?'pink':'blue'
+})
